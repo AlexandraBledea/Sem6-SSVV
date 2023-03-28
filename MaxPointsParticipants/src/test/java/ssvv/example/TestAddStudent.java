fb371878c2c9f8424bf9f12c8be630c70c9d9aca
Bledea Mihaela Alexandra <bledea_alexandra@yahoo.com>
2023-03-28 21:36:49 +0300

    Added Laboratory 1 - In Class + Take Home

--- MaxPointsParticipants/src/test/java/ssvv/example/TestAddStudent.java
+++ MaxPointsParticipants/src/test/java/ssvv/example/TestAddStudent.java
@@ -1,10 +1,10 @@
 package ssvv.example;
 
 import domain.Student;
+import org.junit.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
-import org.junit.jupiter.api.Test;
 import repository.StudentXMLRepo;
 import service.Service;
 import validation.StudentValidator;
@@ -14,6 +14,7 @@ import java.io.BufferedWriter;
 import java.io.File;
 import java.io.FileWriter;
 import java.io.IOException;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 
